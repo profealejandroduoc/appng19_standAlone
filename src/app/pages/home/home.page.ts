986a71771c8bfed0574a7a9186228ac881a64c66
alejandrossm <alejandrossm@outlook.com>
2025-01-08 12:06:00 -0300

    Formulario Funciona

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent,IonText, IonButton } from '@ionic/angular/standalone';
+import {RouterLink} from '@angular/router' //Para navegar importar esta libreria
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonText, IonButton, RouterLink],
 })
 export class HomePage {
   constructor() {}
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonText, IonButtons, IonButton, IonIcon, IonBackButton, 
+  IonItem, IonLabel, IonInput } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonText,IonButtons,IonButton, 
+    IonIcon,IonBackButton, IonItem, IonLabel,IonInput ]
 })
 export class LoginPage implements OnInit {
 
   constructor() { }
 
+  usuario={
+    username:'',
+    password:''
+  }
   ngOnInit() {
   }
+    
+    onSubmit(){
+      console.log("works!!!!");
+    }
 
 }
